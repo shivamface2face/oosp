8284a18bb06f225d87a9cacfb34423c256df7d8a
shivamface2face <shivamfaceuit@gmail.com>
2022-08-17 10:41:43 +0530

    initial

---
+++ src/Arrays/intro.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class intro {
+    public static void main(String[] args) {
+        System.out.println("hello every one array is here");
+//         it is collection of data
+          int [] arr=new int[10];
+          int rool2[]={1,2,3,4};
+          System.out.println(arr[0]);
+    }
+}
